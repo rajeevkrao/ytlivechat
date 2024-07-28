ce93c9761bd2cc173a9315d43ced7f03d99a0233
Rajeev K Rao <rajeevraok@gmail.com>
2024-07-28 13:33:37 +0530

    Nosleep.js test

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -1,27 +1,11 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	/* import NoSleep from 'nosleep.js'; */
-
-	const wakeLock = async() => {
-		if ('wakeLock' in navigator) {
-			const wakeLock = await navigator.wakeLock.request("screen");
-			info = 'Wake Lock is active!';
-			console.log('Wake Lock is active!');
-			return () => wakeLock.release().then(() => console.log('Wake Lock is released.'));
-		} else {
-			info = 'Wake Lock API not supported by this browser.';
-			console.log('Wake Lock API not supported by this browser.');
-		}
-	}
-
-	let info:string = 'test';
-
+	import NoSleep from 'nosleep.js';
 	onMount(() => {
 
-		wakeLock();
-		/* const noSleep = new NoSleep();
+		const noSleep = new NoSleep();
 		noSleep.enable();
-		return () => noSleep.disable(); */
+		return () => noSleep.disable();
 
 	});
 
@@ -39,8 +23,6 @@
 	src="https://rawgit.com/bower-media-samples/big-buck-bunny-480p-30s/master/video.mp4"
 	height="60"
 ></video> -->
-
-{ info }
 
 <main>
 	<iframe src={`https://www.youtube.com/live_chat?v=${params.v}&embed_domain=${hostname}`}></iframe>
